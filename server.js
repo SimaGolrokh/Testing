8b3511fe0fc3224f372599cb121b59881caa1926
sima golrokh bahoush <sima.golrokh@gmail.com>
2025-05-31 12:23:06 +0200

    edit the root

--- server.js
+++ server.js
@@ -8,31 +8,12 @@ const app = express();
 const port = process.env.PORT;
 
 const http = require("http");
-const { Server } = require("socket.io");
-
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"]
-  }
-});
-
-
-const { initSocketIO } = require("./sockets/socketHandler");
-initSocketIO(io);
-require("./listeners/moistureListener");
 
 // routes
 const authRoutes = require("./routes/auth");
-const userRoutes = require("./routes/user");
-const plantRoutes = require("./routes/plant");
-const gardenRoutes = require("./routes/garden");
 const errorHandler = require("./middleware/errorHandler");
-const authenticateIP = require("./middleware/ip");
 const authenticateJWT = require("./middleware/auth");
 const jsonrefresh = require("./routes/auth");
-const sensorRoutes = require("./routes/sensor");
 
 // Middleware
 app.use(cors());
@@ -42,11 +23,7 @@ app.use(authenticateIP);
 app.use(cookieParser());
 // Routes
 app.use("/api/auth", authRoutes);
-app.use("/api/users", authenticateJWT, userRoutes);
-app.use("/api/plants", authenticateJWT, plantRoutes);
-app.use("/api/gardens", authenticateJWT, gardenRoutes);
 app.use("/api/auth", jsonrefresh);
-app.use("/api/sensor", sensorRoutes);
 app.use(errorHandler);
 
 
